ff86d63e6c672c2dd0e2fb20bf55e2b78874f5f6
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-05-17 06:02:29 -0700

    create landing page; fix headers

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const recrawler = require('recrawler');
 const request = require('request');
 const static = require('node-static');
 
-const cacheDir = path.join(__dirname, '.cache');
+const rootDir = path.join(__dirname, 'public');
+const cacheDir = path.join(rootDir, '.cache');
 const host = process.env.TUNNELRUN_FLICKR_HOST || process.env.HOST || '0.0.0.0';
 const port = process.env.TUNNELRUN_FLICKR_PORT || process.env.PORT || 7000;
 
@@ -19,27 +20,16 @@ try {
 } catch (e) {
 }
 
-var staticServer = new static.Server(cacheDir);
+var staticServer = new static.Server(rootDir, {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type'
+  }
+});
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Request-Method', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-  if (req.headers.origin) {
-    res.setHeader('Access-Control-Allow-Headers', req.headers.origin);
-  }
-  if (req.method === 'OPTIONS') {
-    res.writeHead(200);
-    res.end();
-    return;
-  }
-
   const reqPathame = urlParse(req.url).pathname;
-
-  if (!req.url.includes('?') && reqPathame.split('/').length < 3) {
-    staticServer.serve(req, res);
-    return;
-  }
 
   // Example URL formats:
   // - https://www.flickr.com/photos/hamburgerjung/34539117106
@@ -56,26 +46,29 @@ const server = http.createServer((req, res) => {
     photoId = parseInt(matches[2], 10);
   }
 
+  if (!photoUser || !photoId) {
+    staticServer.serve(req, res);
+    return;
+  }
+
   if (photoUser && photoId) {
-    const flickrDownloadUrl = 'https://www.flickr.com/photos/' + photoUser + '/' + photoId + '/sizes/o/';
     const hash = crypto.createHash('sha1').update(flickrDownloadUrl).digest('hex');
     const hashPath = hash + '.jpg';
-    const hashUrl = '/' + hashPath;
+    const hashUrl = '/.cache/' + hashPath;
     const fn = path.join(cacheDir, hashPath);
 
     fs.stat(fn, function (err, stats) {
       if (!err && stats.isFile()) {
         req.url = hashUrl;
-        // console.log('reading from file', req.url);
+        // Read from file.
         staticServer.serve(req, res);
         return;
       }
 
-      // console.log('fetching from network', req.url);
+      // Fetch from network.
       recrawler(flickrDownloadUrl).then($ => {
         $('#allsizes-photo img[src]').each(function () {
           const url = $(this).attr('src');
-          // console.log('requesting', url);
           request(url)
             .pipe(fs.createWriteStream(fn))
             .on('finish', function () {
